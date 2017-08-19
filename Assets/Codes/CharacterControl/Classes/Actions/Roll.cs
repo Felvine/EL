8d7cc5d7ec14cf47d94cf49aca84aa059ae25654
Marteen21 <znmarteen21@gmail.com>
2017-08-19 18:40:45 +0200

    Added ActionSequence

--- Assets/Codes/CharacterControl/Classes/Actions/Roll.cs
+++ Assets/Codes/CharacterControl/Classes/Actions/Roll.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 namespace Actions {
     class Roll : Move {
-        private bool invincibility = false;
-        public Roll (Character characterIn, float durationIn, float distanceIn) : base (characterIn, durationIn, distanceIn / durationIn) {
+        private bool invincibility;
+        public Roll (Character characterIn, float durationIn, float distanceIn, bool invicibilityIn) : base (characterIn, durationIn, distanceIn / durationIn) {
+            this.invincibility = invicibilityIn;
+        }
+        protected override void PreActions () {
+            Debug.Log ("lol");
+            base.PreActions ();
+            if (invincibility) {
+                SpriteRenderer[] spriteRenderers = User.Transform.GetComponentsInChildren<SpriteRenderer> ();
+                foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                    spriteRenderer.color = Color.red;
+                }
+            }
+        }
 
+        protected override void PostActions () {
+            base.PostActions ();
+            if (invincibility) {
+                SpriteRenderer[] spriteRenderers = User.Transform.GetComponentsInChildren<SpriteRenderer> ();
+                foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                    spriteRenderer.color = Color.white;
+                }
+            }
         }
 
         protected override void PerformAction () {
