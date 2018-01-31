b8c892081da765b1ca02a3cb06e60a7940aa5763
unknown <znmarteen21@gmail.com>
2018-01-31 21:12:24 +0100

    Attack based on TriggerStay instead of TriggerEnter

--- Assets/Codes/CharacterControl/Scripts/Other/WeaponBehaviour.cs
+++ Assets/Codes/CharacterControl/Scripts/Other/WeaponBehaviour.cs
@@ -15,22 +15,54 @@ public class WeaponBehaviour : MonoBehaviour {
         }
     }
 
-    void OnTriggerEnter (Collider other) {
+
+    void OnTriggerStay(Collider other)
+    {
         ICharacterController targetController = other.GetComponentInChildren<ICharacterController>();
-        if (user.Properties.IsAttacking) {
-            if (user.Faction == Character.Factions.Player) {
-                if (other.tag != "Player") {
+        ICharacterController userController = GetComponentInParent<ICharacterController>();
+        if (user.Properties.IsAttacking)
+        {
+            if (user.Faction == Character.Factions.Player)
+            {
+                if (other.tag != "Player")
+                {
                     if (other.tag == "Enemy")
                     {
                         targetController.AddEvent(new ReceiveDamageEvent(10));
+                        userController.AddEvent(new SetAttackEvent(false));
                     }
                 }
-            } else if (user.Faction == Character.Factions.Enemy) {
-                if (other.tag == "Player") {
+            }
+            else if (user.Faction == Character.Factions.Enemy)
+            {
+                if (other.tag == "Player")
+                {
                     targetController.AddEvent(new ReceiveDamageEvent(10));
                     targetController.AddEvent(new AddActionEvent(targetController.GetUser().GetAction("Fall")));
+                    userController.AddEvent(new SetAttackEvent(false));
                 }
             }
         }
     }
+
+
+
+    //void OnTriggerEnter (Collider other) {
+    //    ICharacterController targetController = other.GetComponentInChildren<ICharacterController>();
+    //    if (user.Properties.IsAttacking) {
+    //        if (user.Faction == Character.Factions.Player) {
+    //            if (other.tag != "Player") {
+    //                if (other.tag == "Enemy")
+    //                {
+    //                    targetController.AddEvent(new ReceiveDamageEvent(10));
+    //                }
+    //            }
+    //        } else if (user.Faction == Character.Factions.Enemy) {
+    //            if (other.tag == "Player") {
+    //                targetController.AddEvent(new ReceiveDamageEvent(10));
+    //                targetController.AddEvent(new AddActionEvent(targetController.GetUser().GetAction("Fall")));
+    //            }
+    //        }
+    //    }
+    //}
 }
